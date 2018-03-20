a4d8c9cebdab29f6b47c6c607b1d18b05ebc4c9b
Dylan Arcand <dylanarcand@hotmail.com>
2018-03-19 19:39:09 -0700

    Removed Randomized Item script and added GameManager script. Implemented random index function in GameManager which takes a game list. Updated gitIgnore

---
+++ Big Fishing 2018/Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public int RandomIndex(List<GameObject> list)
+    {
+        Random.InitState((int)System.DateTime.Now.Millisecond + System.DateTime.Now.Minute);
+
+        return Random.Range(0, list.Count);
+    }
+}
