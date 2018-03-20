e82adb7102776e05a734530d5664b7cf631e2142
Dylan Arcand <dylanarcand@hotmail.com>
2018-03-19 19:20:38 -0700

    Uploading game with the fishing line casting and reeling implemented

---
+++ Big Fishing 2018/Assets/Scripts/CatchingScript.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatchingScript : MonoBehaviour {
+
+    private const float _SecondsToMinuteConversion = 60f;
+
+    public float FishingBuffer = 10f;
+    public float MinimumWait = 0.5f;
+    public float MaximumWait = 4.0f;
+    public AudioSource AudioSource;
+
+    [HideInInspector]
+    public bool FishCatchable = false;
+
+    private float _TimeUntilBite;
+
+    private void Awake()
+    {
+        AudioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnEnable()
+    {
+        RandomTimeCreation();
+    }
+
+    void Update ()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            Debug.Log("Time remaining: " + (_TimeUntilBite - Time.time));
+        }
+
+		if(Time.time > _TimeUntilBite && Time.time - _TimeUntilBite < FishingBuffer)
+        {
+            if (AudioSource != null)
+            {
+                if (!FishCatchable)
+                {
+                    FishCatchable = true;
+                    AudioSource.Play();
+                }
+            }
+        }
+
+        if(Time.time > _TimeUntilBite + FishingBuffer)
+        {
+            FishCatchable = false;
+            RandomTimeCreation();
+        }
+	}
+
+    void RandomTimeCreation()
+    {
+        Random.InitState((int)System.DateTime.Now.Millisecond + System.DateTime.Now.Minute);
+        float _WhenToBite = Random.Range(MinimumWait, MaximumWait) * _SecondsToMinuteConversion;
+        _TimeUntilBite = Time.time + _WhenToBite;
+        Debug.Log("Time until fish is active: " + _WhenToBite);
+    }
+}
---
+++ Big Fishing 2018/Assets/Scripts/ThrowingLine.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowingLine : MonoBehaviour
+{
+    public GameObject FishingLine;
+    public CatchingScript FishStatus;
+
+    private float CastBuffer = 0.001f;
+    private float _CastBufferTime;
+    private bool LineCasted = false;
+
+    private void Awake()
+    {
+        _CastBufferTime = CastBuffer;
+
+        if (FishingLine != null)
+        {
+            FishStatus = FishingLine.GetComponent<CatchingScript>();
+        }
+
+        if(FishStatus == null)
+        {
+            Debug.Log("Failed to load FishStatus");
+        }
+    }
+	
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0) && !LineCasted && (Time.time > _CastBufferTime))
+        {
+            LineCasted = true;
+
+            if (FishingLine != null)
+            {
+                FishingLine.SetActive(true);
+                Debug.Log("Fishing Line has been thrown");
+            }
+
+            else
+            {
+                Debug.Log("Fishing Line has not been added in inspector");
+            }
+
+            _CastBufferTime = Time.time + CastBuffer;
+        }
+
+        if(FishingLine != null && LineCasted)
+        {
+            if (FishStatus != null)
+            {
+                if (Input.GetKeyDown(KeyCode.Mouse0) && (Time.time > _CastBufferTime))
+                {
+                    if (FishStatus.FishCatchable == true)
+                    {
+                        Debug.Log("Fish was successfully caught!");
+                    }
+
+                    Debug.Log("Reeling line back in");
+
+                    FishingLine.SetActive(false);
+                    LineCasted = false;
+
+                    _CastBufferTime = Time.time + CastBuffer;
+                }
+            }
+        }
+	}
+}
