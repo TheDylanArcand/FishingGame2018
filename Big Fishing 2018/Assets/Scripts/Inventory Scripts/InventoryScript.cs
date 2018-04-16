4bbaa38b451982f0086db51c9d08acbe19bd752b
DYLANARCAND\dylan <dylanarcand@hotmail.com>
2018-04-16 14:25:11 -0700

    Modified Item Prefabs and added additional scripts relating to the inventory

--- Big Fishing 2018/Assets/Scripts/Inventory Scripts/InventoryScript.cs
+++ Big Fishing 2018/Assets/Scripts/Inventory Scripts/InventoryScript.cs
@@ -1,17 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventoryScript : MonoBehaviour
 {
-    
-	void Start ()
+    public const int NumberOfItemSlots = 4;
+
+    public RawImage[] ItemImages = new RawImage[NumberOfItemSlots];
+    public ItemScript[] Items = new ItemScript[NumberOfItemSlots];
+
+    public void AddItem(ItemScript itemToAdd)
     {
-		
-	}
-	
-	void Update ()
+        for (int i = 0; i < Items.Length; i++)
+        {
+            if(Items[i] == null)
+            {
+                Items[i] = itemToAdd;
+                ItemImages[i] = itemToAdd.RawImage;
+                return;
+            }
+        }
+    }
+
+    public void RemoveItem(ItemScript itemToRemove)
     {
-		
-	}
+        for (int i = 0; i < Items.Length; i++)
+        {
+            if (Items[i] == itemToRemove)
+            {
+                Items[i] = null;
+                ItemImages[i] = null;
+                return;
+            }
+        }
+    }
 }
